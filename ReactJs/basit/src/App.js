cd5bf0496116d98746d91fb229974ddd0e82f59d
basitdurrani <basitdurrani05786@gmail.com>
2025-05-28 19:36:28 +0530

    Updated

---
+++ ReactJs/basit/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+
+   <>
+
+   <Navbar title="Avengers"/>
+
+   <div className='Footer'>
+    CopyRight @2025 Avengers
+   </div>
+
+
+
+
+
+
+   
+ 
+   </>
+  
+
+
+  );
+}
+
+export default App;
---
+++ ReactJs/basit/src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Navbar(props) {
+
+    // let chandni = 35   class based components
+  return (
+    <>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">{props.title}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Captain America</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Thanos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Loki</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Iron-Man</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+
+    </>
+  
+
+
+  );
+}
+
+export default Navbar;
